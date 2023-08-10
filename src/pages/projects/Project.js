a6727543eb54cecde2f35e7146251a4835aa10ee
mathesh <matheshyogeswaran@gmail.com>
2023-08-10 08:22:25 +0530

    add github link

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+
 const Project = () => {
     const { projectId } = useParams();
     const [project, setProject] = useState(null);
@@ -20,18 +21,6 @@ const Project = () => {
     const [softwareArchitectName, setSoftwareArchitectName] = useState('');
     const [teamLeadName, setTeamLeadName] = useState('');
 
-    const fetchUserName = (userId, setNameFunction) => {
-        axios.get(`${process.env.REACT_APP_API_BASE}/users/${userId}`)
-            .then(response => {
-                const userData = response.data.data;
-                if (userData) {
-                    const fullName = `${userData.firstName} ${userData.lastName}`;
-                    setNameFunction(fullName);
-                }
-            })
-            .catch(error => console.error('Error fetching user:', error));
-    };
-
     useEffect(() => {
         if (project) {
             if (project.projectManager) {
@@ -46,13 +35,24 @@ const Project = () => {
         }
     }, [project]);
 
+    const fetchUserName = (userId, setNameFunction) => {
+        axios.get(`${process.env.REACT_APP_API_BASE}/users/${userId}`)
+            .then(response => {
+                const userData = response.data.data;
+                if (userData) {
+                    const fullName = `${userData.firstName} ${userData.lastName}`;
+                    setNameFunction(fullName);
+                }
+            })
+            .catch(error => console.error('Error fetching user:', error));
+    };
+
     return (
         <div className="container mt-4">
-            <h1>Project Details</h1>
+            <h1 className="display-4 mb-4">Project Details</h1>
             {project ? (
                 <div className="card">
                     <div className="card-body">
-                        {/* <h3 className="card-title">Project ID: {project._id}</h3> */}
                         <div className="row">
                             <div className="col-md-9">
                                 <h3 className="card-title">Project Name: {project.projectName}</h3>
@@ -65,14 +65,20 @@ const Project = () => {
                             <div className="col-md-6">
                                 <p className="card-text">Project Description: {project.projectDescription}</p>
                             </div>
+                            <div className="col-md-6">
+                                <p className="card-text">Client Name: {project.clientName}</p>
+                                <p className="card-text">Client Address: {project.clientAddress}</p>
+                                <p className="card-text">Client Email: {project.clientContactEmail}</p>
+                                <p className="card-text">Client Number: {project.clientPhoneNumber}</p>
+                            </div>
                         </div>
-                        <h4>Developers:</h4>
-                        <ul>
+                        <h4 className="mt-4">Developers:</h4>
+                        <ul className="list-group">
                             {project.developers.map(developer => (
-                                <li key={developer.value}>{developer.label}</li>
+                                <li key={developer.value} className="list-group-item">{developer.label}</li>
                             ))}
                         </ul>
-                        <h6>Project Manager: {projectManagerName}</h6>
+                        <h6 className="mt-4">Project Manager: {projectManagerName}</h6>
                         <h6>Software Architect: {softwareArchitectName}</h6>
                         <h6>Team Lead: {teamLeadName}</h6>
                     </div>
