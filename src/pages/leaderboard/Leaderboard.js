990ab8a275efba9eb1a7c598abe950d16882e7a3
mathesh <matheshyogeswaran@gmail.com>
2023-08-10 20:10:34 +0530

    leaderboard image

---
+++ src/pages/leaderboard/Leaderboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import backgroundImage from './lead.png';
+
+function Leaderboard() {
+    return (
+        <div className="full-image-container">
+            <img
+                src={backgroundImage}
+                alt="Full Background"
+                className="full-image"
+                style={{ width: '90%' }} // Use correct syntax for inline style
+            />
+        </div>
+    );
+}
+
+export default Leaderboard;
